67d032ea881d7e85ed42d5c8db76bdc22702130f
Anmol Shukla <anmolshukla743@gmail.com>
2025-07-05 22:33:49 +0530

    Removed <Header /> from # page; made some changes to give better the page look

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
+    
     <html lang="en">
       <body className={inter.className}>
       <Toaster />
--- app/page.js
+++ app/page.js
@@ -8,22 +8,22 @@ export default function Home() {
     <div>
       {/* <Image src={'/grid.svg'} className="absolute z-[-10] opacity-75 w-full" 
       width={1200} height={300} /> */}
-      <Header/>
+      
      <section className=" z-50">
-    <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+    <div className="py-11 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
         <a href="#" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
             <span className="text-xs bg-black rounded-full text-white px-4 py-1.5 mr-3">New</span> <span className="text-sm font-medium">AnmolShukla.Hello</span> 
             <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
         </a>
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl dark:text-white">Your Personal Ai Interview Coach</h1>
         <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Double your chances of landing that job offer with our AI-powered interview prep</p>
-        <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+        <div className="flex mb-8 lg:mb-16 flex-row justify-center space-y-0 space-x-4">
             <a href="/dashboard" className="inline-flex justify-center items-center py-3 px-5 text-base font-semibold text-center text-black hover:transition rounded-lg bg-primary hover:bg-primary focus:ring-4 focus:ring-gray-100 dark:focus:ring-primary-900">
                 Get Started
                 <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
             <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-semibold text-center text-white rounded-lg hover:bg-white hover:text-black hover:fill-black focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                <svg className="mr-2 -ml-1 w-5 h-5 fill-white" fill="hover:black" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
+                <svg className="mr-2 -ml-1 w-5 h-5 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
                 Watch video
             </a>  
         </div>
@@ -73,7 +73,7 @@ export default function Home() {
          hover:border-pink-500/10 hover:shadow-pink-500/10"
         href="#"
       >
-       <AtomIcon className='h-8 fill-white w-8'/>
+       <AtomIcon className='h-8 stroke-white w-8'/>
 
         <h2 className="mt-4 text-xl font-bold text-white">Write promot for your form</h2>
 
@@ -87,7 +87,7 @@ export default function Home() {
         className="block rounded-xl border bg-secondary p-8 shadow-xl transition hover:border-gray-500/10"
         href="#"
       >
-      <Edit className='h-8 fill-white w-8'/>
+      <Edit className='h-8 stroke-white w-8'/>
 
         <h2 className="mt-4 text-xl font-bold text-white">Edit Your form </h2>
 
@@ -101,7 +101,7 @@ export default function Home() {
         className="block rounded-xl border bg-secondary border-gray-200 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
         href="#"
       >
-      <Share2 className='h-8 fill-white w-8' />
+      <Share2 className='h-8 stroke-white w-8' />
 
         <h2 className="mt-4 text-xl font-bold text-white">Share & Start Accepting Responses</h2>
 
