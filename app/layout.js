95c2459b71f701345a249982a51bf788455fe8c5
Anmol Shukla <122140970+1441087236-360@users.noreply.github.com>
2025-07-18 15:07:32 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,8 @@ export default function RootLayout({ children }) {
       <Toaster />
       {children}</body>
     </html>
-    </ClerkProvider>
     <Analytics />
+    </ClerkProvider>
+    
   );
 }
