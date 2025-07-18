9b828e6f2b3e36f3a47dd4e087922172442bb68c
Anmol Shukla <122140970+1441087236-360@users.noreply.github.com>
2025-07-18 15:31:14 +0530

    Update Header.jsx

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -14,8 +14,15 @@ function Header() {
       name: "Dashboard",
       href: "/dashboard",
       icon: (
-        <svg width="28" height="28" fill="none" stroke="currentColor" strokeWidth="3" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M3 12l2-2m0 0l7-7 7 7m-9 2v8m4-8v8m5 0a2 2 0 002-2V7a2 2 0 00-.586-1.414l-7-7a2 2 0 00-2.828 0l-7 7A2 2 0 003 7v11a2 2 0 002 2h3" />
+       
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="none" viewBox="0 0 24 24" width="28" height="28" preserveAspectRatio="xMidYMid meet" style="width: 100%; height: 100%; transform: translate3d(0px, 0px, 0px); content-visibility: visible;">
+        <defs>
+          <clipPath id="__lottie_element_291">
+            <rect width="512" height="512" x="0" y="0"></rect>
+          </clipPath>
+        </defs>
+        <g clip-path="url(#__lottie_element_291)">
+        <g transform="matrix(1,0,0,1,-17,-41.4639892578125)" opacity="1" style="display: block;"><g opacity="1" transform="matrix(1,0,0,1,273,295.73199462890625)"><path stroke-linecap="round" stroke-linejoin="round" fill-opacity="0" stroke="rgb(0,0,0)" stroke-opacity="1" stroke-width="42" d=" M153.65699768066406,-66.62799835205078 C153.65699768066406,-66.62799835205078 27.656999588012695,-176.8780059814453 27.656999588012695,-176.8780059814453 C11.822999954223633,-190.73199462890625 -11.822999954223633,-190.73199462890625 -27.656999588012695,-176.8780059814453 C-27.656999588012695,-176.8780059814453 -153.65699768066406,-66.62799835205078 -153.65699768066406,-66.62799835205078 C-162.77099609375,-58.652000427246094 -168,-47.12799835205078 -168,-35.018001556396484 C-168,-35.018001556396484 -168,148.73199462890625 -168,148.73199462890625 C-168,171.9290008544922 -149.19700622558594,190.73199462890625 -126,190.73199462890625 C-126,190.73199462890625 126,190.73199462890625 126,190.73199462890625 C149.1959991455078,190.73199462890625 168,171.9290008544922 168,148.73199462890625 C168,148.73199462890625 168,-35.018001556396484 168,-35.018001556396484 C168,-47.12799835205078 162.77099609375,-58.652000427246094 153.65699768066406,-66.62799835205078z"></path></g></g><g transform="matrix(1,0,0,1,88,172)" opacity="1" style="display: block;"><g opacity="1" transform="matrix(1,0,0,1,168,189)"><path stroke-linecap="round" stroke-linejoin="round" fill-opacity="0" stroke="rgb(0,0,0)" stroke-opacity="1" stroke-width="42" d=" M63,84 C63,84 -63,84 -63,84 C-63,84 -63,-42 -63,-42 C-63,-65.1969985961914 -44.196998596191406,-84 -21,-84 C-21,-84 21,-84 21,-84 C44.19599914550781,-84 63,-65.1969985961914 63,-42 C63,-42 63,84 63,84z"></path></g></g></g>
         </svg>
       ),
     },
